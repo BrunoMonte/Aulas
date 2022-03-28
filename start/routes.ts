8f82ae1b7350dd28f46f6024daec3235b37c6e2c
Bruno Monte <brunoaguiarprofissional@gmail.com>
2022-03-28 14:30:03 -0300

    Starting project registration classes

---
+++ start/routes.ts
@@ -0,0 +1,9 @@
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.post('/users', '')
+Route.post('/classes', '')
+Route.get('/classes', '')
+Route.get('/classes/:id', '')
+Route.put('/classes', '')
+Route.delete('/classes/:id', '')
+Route.post('/classes/comments', '')
