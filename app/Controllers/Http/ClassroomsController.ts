c8d2a9966a90131531d8a0b704e2321201d2a843
Bruno Monte <brunoaguiarprofissional@gmail.com>
2022-04-01 10:22:54 -0300

    route adjustment

--- app/Controllers/Http/ClassroomsController.ts
+++ app/Controllers/Http/ClassroomsController.ts
@@ -3,6 +3,11 @@ import Classroom from '../../Models/Classroom'
 
 export default class ClassroomsController {
 
+  public async index({ response }: HttpContextContract) {
+      const classroom = await Classroom.all()
+      return response.ok(classroom)
+  }
+
   public async create({ request, response }: HttpContextContract) {
     const { name } = request.all()
     const classroom = new Classroom()
@@ -14,11 +19,28 @@ export default class ClassroomsController {
     return response.status(200).json(classroom)
   }
 
-  public async store({}: HttpContextContract) {}
+  public async store({  }: HttpContextContract) {
+    
+  }
 
-  public async show({}: HttpContextContract) {}
+  public async show({ params, response }: HttpContextContract) {
+    const { id } = params
+    const classroom = await Classroom.find(id)
 
-  public async edit({}: HttpContextContract) {}
+    return response.json(classroom)
+  }
+
+  public async edit({ params, request, response }: HttpContextContract) {
+    const { id } = params
+    const { name } = request.all()
+    const classroom:any = await Classroom.find(id)
+
+    classroom.name = name || classroom.name
+
+    await classroom.save()
+
+    return response.json(classroom)
+  }
 
   public async update({}: HttpContextContract) {}
 
--- start/routes.ts
+++ start/routes.ts
@@ -2,8 +2,8 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.post('/users', 'UsersController.create')
 Route.post('/classes', 'ClassroomsController.create')
-Route.get('/classes', '')
-Route.get('/classes/:id', '')
+Route.get('/classes', 'ClassroomsController.index')
+Route.get('/classes/:id', 'ClassroomsController.show')
 Route.put('/classes', '')
 Route.delete('/classes/:id', '')
 Route.post('/classes/comments', '')
